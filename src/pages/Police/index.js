f2f892937f9f78df63f8ba528048eceb59610548
Daniel Felizardo <daniel.david772@gmail.com>
2019-05-10 04:40:23 -0300

    feat: add screen of user our police

--- src/pages/Police/index.js
+++ src/pages/Police/index.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends Component {
+  static navigationOptions = {
+    title: "Police",
+    headerTransparent: true,
+    headerTintColor: "#fff"
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Secuty Total!</Text>
+        <Text style={styles.welcome}>Welcome to police!</Text>
       </View>
     );
   }
@@ -16,6 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "#059"
   }
 });
---
+++ src/pages/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #059;
+`;
+
+export const Title = styled.Text`
+  margin-top: 50px;
+  color: #fff;
+  font-size: 30;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background-color: #fff;
+  width: 150;
+  height: 40;
+  border-radius: 50;
+  margin-top: 30;
+  justify-content: center;
+  align-items: center;
+`;
+export const ButtonText = styled.Text`
+  font-size: 16px;
+  color: #000;
+`;

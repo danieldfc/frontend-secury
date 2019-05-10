e6a4fa0f16390a90384d0c0978975919f4acf328
Daniel Felizardo <daniel.david772@gmail.com>
2019-05-10 12:17:40 -0300

    feat: refatoring code

--- src/pages/Police/index.js
+++ src/pages/Police/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Container, Title } from "./styles";
 
 export default class App extends Component {
   static navigationOptions = {
@@ -9,18 +9,9 @@ export default class App extends Component {
   };
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to police!</Text>
-      </View>
+      <Container style={styles.container}>
+        <Title style={styles.welcome}>Welcome to police!</Title>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#059"
-  }
-});

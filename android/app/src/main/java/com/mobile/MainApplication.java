d4c13e5f7d95ac50ada4e69d2ea67f84b02bf0e1
Daniel Felizardo <daniel.david772@gmail.com>
2019-05-14 04:22:32 -0300

    feat: add screen user

--- android/app/src/main/java/com/mobile/MainApplication.java
+++ android/app/src/main/java/com/mobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactNativeQuickActions.AppShortcutsPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new AppShortcutsPackage(),
             new AsyncStoragePackage(),
             new RNGestureHandlerPackage()
--- src/pages/User/styles.js
+++ src/pages/User/styles.js
@@ -1,4 +1,8 @@
 import styled from "styled-components/native";
+import { Dimensions } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+
+const { width: WIDTH } = Dimensions.get("window");
 
 export const Container = styled.View`
   flex: 1;
@@ -8,34 +12,63 @@ export const Container = styled.View`
 `;
 
 export const Title = styled.Text`
-  font-size: 30px;
+  font-size: 20px;
   color: #fff;
-  margin-bottom: 50px;
   font-family: sans-serif;
+  font-weight: bold;
+  margin-bottom: 50px;
+  margin-top: 10px;
+  opacity: 0.5;
 `;
 
 export const Input = styled.TextInput`
-  width: 300px;
-  height: 40px;
-  background-color: #fff;
-  margin: 10px;
-  padding: 10px;
+  width: WIDTH - 55;
+  height: 45px;
+  background-color: rgba(0, 0, 0, 0.35);
+  color: rgba(255, 255, 255, 0.7);
   font-size: 16px;
-  color: #000;
-  border-radius: 50px;
+  border-radius: 45px;
+  margin-right: 25px;
+  padding-left: 10px;
 `;
 
 export const Button = styled.TouchableOpacity`
-  margin: 20px 0;
-  background-color: #222;
-  width: 300px;
-  height: 40px;
+  width: WIDTH - 55;
+  height: 45px;
+  background-color: #432577;
+  font-size: 16px;
+  margin-top: 20px;
   justify-content: center;
   align-items: center;
-  border-radius: 50px;
 `;
 
 export const ButtonText = styled.Text`
   font-size: 16px;
-  color: #fff;
+  color: rgba(255, 255, 255, 0.7);
+  text-align: center;
+`;
+
+export const ImageContainer = styled.View`
+  align-items: center;
+  margin-bottom: 50px;
+`;
+
+export const Image = styled.Image`
+  width: 120px;
+  height: 120px;
+`;
+
+export const InputContainer = styled.InputContainer`
+  margin-top: 10px;
+`;
+export const Icon = styled.Icon`
+  position: absolute;
+  top: 10px;
+  left: 37px;
+`;
+
+export const EyeButton = styled.TouchableOpacity`
+  position: absolute;
+  top: 10px;
+  right: 37px;
 `;

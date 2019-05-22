7caa1fd7b2c19413a788763a258c669b51433b49
Daniel Felizardo <daniel.david772@gmail.com>
2019-05-22 17:58:56 -0300

    feat: adicionado quadro rolável para o usuário

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,25 +1,88 @@
 import React, { Component } from "react";
-import { Container, Button, ButtonText } from "./styles";
-import { Alert } from "react-native";
+
+import { Animated } from "react-native";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import {
+  Container,
+  Button,
+  Content,
+  ButtonText,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Card,
+  Title,
+  Description,
+  Annotation
+} from "./styles";
+
+const offset = 0;
+const translateY = new Animated.Value(0);
+const animatedEvent = Animated.event(
+  [
+    {
+      nativeEvent: {
+        translateY: translateY
+      }
+    }
+  ],
+  {
+    useNativeDriver: true
+  }
+);
+const { onLocationSelected } = this.props;
 
 export default class Search extends Component {
-  state = {
-    searchFocused: false
+  onHandlerStateChange = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+    }
   };
 
   render() {
-    const { onLocationSelected } = this.props;
-    const { searchFocused } = this.state;
-
     return (
       <Container>
-        <Button
-          onPress={() => {
-            Alert.alert("oi");
-          }}
-        >
-          <ButtonText>Solicitar</ButtonText>
-        </Button>
+        <Content>
+          <PanGestureHandler
+            onGestureEvent={animatedEvent}
+            onHandlerStateChange={this.onHandlerStateChange}
+          >
+            <Card
+              style={{
+                transform: [
+                  {
+                    translateY: translateY.interpolate({
+                      inputRange: [-350, 0, 380],
+                      outputRange: [-50, 0, 380],
+                      extrapolate: "clamp"
+                    })
+                  }
+                ]
+              }}
+            >
+              <CardHeader>
+                <Icon name="attach-money" size={28} color="#666" />
+                <Icon name="visibility-off" size={28} color="#666" />
+              </CardHeader>
+              <CardContent>
+                <Title>User</Title>
+                <Description>Descrição</Description>
+              </CardContent>
+              <CardFooter>
+                <Button
+                  onPress={() => {
+                    Alert.alert("oi");
+                  }}
+                >
+                  <ButtonText>Solicitar</ButtonText>
+                </Button>
+                <Annotation>Essa é a seção de criação de ocorrência</Annotation>
+              </CardFooter>
+            </Card>
+          </PanGestureHandler>
+        </Content>
       </Container>
     );
   }

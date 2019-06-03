8aa50093a3306c8bf6a5d5b190cd99404f51e7da
Daniel Felizardo <daniel.david772@gmail.com>
2019-06-03 02:32:02 -0300

    feat: with refreshing in tasks

--- src/pages/Task/index.js
+++ src/pages/Task/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, RefreshControl } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import AsyncStorage from "@react-native-community/async-storage";
 import api from "../../services/api";
@@ -12,7 +12,16 @@ import {
   ListItem,
   TitleList,
   TitleContent,
-  EmailContent
+  EmailContent,
+  ButtonView,
+  ButtonDelete,
+  ButtonDeleteText,
+  ButtonUpdate,
+  ButtonUpdateText,
+  DescriptionListFound,
+  ListFound,
+  TitleListFound,
+  ListText
 } from "./styles";
 
 const tabBarIcon = name => ({ tintColor }) => {
@@ -40,12 +49,24 @@ export default class Task extends Component {
   state = {
     errorMessage: null,
     tasks: [],
-    user: {}
+    user: {},
+    editable: false,
+    title: null,
+    description: null,
+    refreshing: false
   };
 
   async componentDidMount() {
     this.getTaskList();
+    this._onRefresh();
   }
+
+  _onRefresh = () => {
+    this.setState({ refreshing: true });
+    this.getTaskList().then(() => {
+      this.setState({ refreshing: false });
+    });
+  };
 
   getTaskList = async () => {
     try {
@@ -59,21 +80,97 @@ export default class Task extends Component {
     }
   };
 
+  handleDeleteTask = async id => {
+    await api.delete(`/task/${id}`);
+    alert("Task deletada com sucesso!");
+    this.setState({ refreshing: false });
+    this._onRefresh();
+  };
+
+  handleUpdateTask = async id => {
+    const { title, description } = this.state;
+    if (title === null || description === null) {
+      alert("Você não alterou os campos.");
+      this.showPass();
+    } else {
+      await api.put(`/task/${id}`, { title, description });
+      alert("Task atualizada com sucesso!");
+      this.showPass();
+      this.setState({ refreshing: false, title: null, description: null });
+      this._onRefresh();
+    }
+  };
+
+  showPass = () => {
+    const { editable } = this.state;
+    if (editable === false) {
+      this.setState({ editable: true });
+    } else if (editable === true) {
+      this.setState({ editable: false });
+    }
+  };
+
   render() {
-    const { tasks, user } = this.state;
-    let score = tasks.map((item, index) => {
+    const { tasks, user, editable, refreshing } = this.state;
+    let score = tasks.map(task => {
       return (
-        <ListItem key={index}>
-          <TitleList>Title: {item.title}</TitleList>
-          <DescriptionList>Description: {item.description}</DescriptionList>
+        <ListItem key={task._id}>
+          <ListText style={{ marginBottom: 3 }}>Title</ListText>
+          <TitleList
+            placeholder={task.title}
+            editable={editable}
+            onChangeText={title => this.setState({ title })}
+          />
+          <ListText style={{ marginTop: 5, marginBottom: 3 }}>
+            Descritpion
+          </ListText>
+          <DescriptionList
+            placeholder={task.description}
+            editable={editable}
+            onChangeText={description => this.setState({ description })}
+          />
+          <ButtonView>
+            <ButtonDelete onPress={() => this.handleDeleteTask(task._id)}>
+              <ButtonDeleteText>Deletar</ButtonDeleteText>
+            </ButtonDelete>
+            {editable ? (
+              <ButtonUpdate onPress={() => this.handleUpdateTask(task._id)}>
+                <ButtonUpdateText>Update</ButtonUpdateText>
+              </ButtonUpdate>
+            ) : (
+              <ButtonUpdate onPress={this.showPass}>
+                <ButtonUpdateText>Edit</ButtonUpdateText>
+              </ButtonUpdate>
+            )}
+          </ButtonView>
         </ListItem>
       );
     });
     return (
       <Container>
-        <TitleContent>Task of user</TitleContent>
+        <TitleContent>Tasks of user</TitleContent>
         <EmailContent>Email: {user.email}</EmailContent>
-        <Content>{score}</Content>
+        <Content
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={this._onRefresh.bind(this)}
+            />
+          }
+        >
+          {score && (
+            <>
+              {score}
+              <ListFound>
+                <TitleListFound>Occurence not found</TitleListFound>
+                <DescriptionListFound>
+                  Don't you have occurrence registered, create a new back to the
+                  map.
+                </DescriptionListFound>
+              </ListFound>
+            </>
+          )}
+        </Content>
       </Container>
     );
   }
--- src/pages/Task/styles.js
+++ src/pages/Task/styles.js
@@ -1,5 +1,43 @@
 import styled from "styled-components/native";
 
+export const ButtonView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ButtonDelete = styled.TouchableOpacity`
+  height: auto;
+  flex: 1;
+  align-items: center;
+  padding: 7px;
+  border: 1px solid #f00;
+  margin-top: 3px;
+  border-radius: 20px;
+`;
+
+export const ButtonDeleteText = styled.Text`
+  color: #f00;
+  font-weight: bold;
+  font-size: 15px;
+`;
+
+export const ButtonUpdate = styled.TouchableOpacity`
+  height: auto;
+  flex: 1;
+  align-items: center;
+  padding: 7px;
+  border: 1px solid #059;
+  margin-top: 3px;
+  border-radius: 20px;
+`;
+
+export const ButtonUpdateText = styled.Text`
+  color: #059;
+  font-weight: bold;
+  font-size: 15px;
+`;
+
 export const Completed = styled.Text`
   color: black;
 `;
@@ -33,14 +71,45 @@ export const ListItem = styled.View`
   margin-top: 10px;
   border-radius: 20px;
 `;
+export const ListFound = styled.View`
+  flex: 1;
+  padding: 20px;
+  border: 1px solid #059;
+  margin-top: 10px;
+  border-radius: 20px;
+`;
 
-export const TitleList = styled.Text`
+export const TitleList = styled.TextInput`
+  font-size: 18px;
+  color: #333;
+  font-weight: bold;
+  border: 1px solid #059;
+  border-radius: 10px;
+  padding: 10px;
+`;
+
+export const ListText = styled.Text`
+  color: #222;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const DescriptionList = styled.TextInput`
+  font-size: 16px;
+  color: #999;
+  margin-top: 10px;
+  border: 1px solid #059;
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 3px;
+`;
+export const TitleListFound = styled.Text`
   font-size: 20px;
   color: #333;
   font-weight: bold;
 `;
 
-export const DescriptionList = styled.Text`
+export const DescriptionListFound = styled.Text`
   font-size: 18px;
   color: #999;
   margin-top: 10px;
